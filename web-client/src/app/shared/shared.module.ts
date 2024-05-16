3c38b5aea0579bb551f2cee4fb7cc933e9c7aef6
Patrick Rawlings <ptyrawl@gmail.com>
2024-05-15 22:41:59 -0400

    Build w/ Angular

--- web-client/src/app/shared/shared.module.ts
+++ web-client/src/app/shared/shared.module.ts
@@ -1,10 +1,31 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTabsModule } from '@angular/material/tabs';
+import {
+  MatSnackBarModule,
+  MAT_SNACK_BAR_DEFAULT_OPTIONS,
+} from '@angular/material/snack-bar';
+import { MatNativeDateModule } from "@angular/material/core";
 
 @NgModule({
   declarations: [ ],
   imports: [
+    CommonModule,
     FormsModule,
     MatCardModule,
     MatCheckboxModule,
--- web-client/tailwind.config.js
+++ web-client/tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
