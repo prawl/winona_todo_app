c3f30ee686ad7d008df0e34dd5474f5655bccf6e
Patrick Rawlings <ptyrawl@gmail.com>
2024-05-17 03:13:31 -0400

    Fix canidatate min length

--- Api/TodoApi/Models/CandidateTodoItem.cs
+++ Api/TodoApi/Models/CandidateTodoItem.cs
@@ -5,7 +5,7 @@ namespace TodoApi.Models
     public class CandidateTodoItem
     {
         [Required]
-        [MinLength(5)]
+        [MinLength(2)]
         [MaxLength(100)]
         public required string Task { get; set; }
         public required string Deadline { get; set; }
