702047b7ffb1cbc15e0eb36eac53b21a6ae13826
Patrick Rawlings <ptyrawl@gmail.com>
2024-05-17 18:24:50 -0400

    Use env vars

--- web-client/src/config/env.config.ts
+++ web-client/src/config/env.config.ts
@@ -1,31 +1,31 @@
 export const env: { env: 'local' | 'dev' | 'qa' | 'prod' } = {
-  env: 'local',
-};
-
-
-export interface EnvironmentConfig {
-
-  apiUrl: string;
-}
-
-export interface Environments {
-  local: EnvironmentConfig;
-  dev: EnvironmentConfig;
-  qa: EnvironmentConfig;
-  prod: EnvironmentConfig;
-}
-
-export const environments: Environments = {
-  local: {
-    apiUrl: 'https://localhost:7120/api/TodoItems',
-  },
-  dev: {
-    apiUrl: 'https://localhost:7120/api/TodoItems',
-  },
-  qa: {
-    apiUrl: 'https://localhost:7120/api/TodoItems',
-  },
-  prod: {
-    apiUrl: 'https://localhost:7120/api/TodoItems',
+    env: 'local',
+  };
+  
+  
+  export interface EnvironmentConfig {
+  
+    apiUrl: string;
   }
-};
+  
+  export interface Environments {
+    local: EnvironmentConfig;
+    dev: EnvironmentConfig;
+    qa: EnvironmentConfig;
+    prod: EnvironmentConfig;
+  }
+  
+  export const environments: Environments = {
+    local: {
+      apiUrl: 'https://localhost:7120/api/TodoItems',
+    },
+    dev: {
+      apiUrl: 'https://localhost:7120/api/TodoItems',
+    },
+    qa: {
+      apiUrl: 'https://localhost:7120/api/TodoItems',
+    },
+    prod: {
+      apiUrl: 'https://localhost:7120/api/TodoItems',
+    }
+  };
