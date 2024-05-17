9d8005a6e81be0b5047b0b83f555f2bcb7823cac
Patrick Rawlings <ptyrawl@gmail.com>
2024-05-17 17:21:19 -0400

    Update assembly

--- Api/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ Api/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -16,6 +16,7 @@ using System.Reflection;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3925d7676e7d03dcc50195a8567ed46482bec6b8")]
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae6d7551b88d7b8019e8e1b1c7e0b7e7c194caad")]
@@ -26,6 +27,9 @@ using System.Reflection;
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+001029c550cb8ac6334b9dba993d87e466a67710")]
 >>>>>>> e0ee22f (Removed DTO)
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0ee22f3211b6d07f81c0400ff0d2216024d0ee2")]
+>>>>>>> bd52f45 (Merge)
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
